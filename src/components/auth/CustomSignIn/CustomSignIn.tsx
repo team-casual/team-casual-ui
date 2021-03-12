c9b8da5762ee6f6525298bae9438feb11cfc4106
werzl <adam@houseofhewitt.co.uk>
2021-03-12 14:48:16 +0000

    Login - moved inputs and button into a form component

--- src/components/auth/CustomSignIn/CustomSignIn.tsx
+++ src/components/auth/CustomSignIn/CustomSignIn.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import { Auth } from "aws-amplify";
 import { IAuthenticatorProps } from "aws-amplify-react/lib-esm/Auth/Authenticator";
-import { Container, Row, Col, Card, InputGroup, FormControl } from "react-bootstrap";
+import { Container, Row, Col, Card, InputGroup, FormControl, Button } from "react-bootstrap";
 
 import logo from '../../../logo.svg';
 
@@ -11,7 +11,9 @@ export const CustomSignIn = (props: IAuthenticatorProps) => {
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const signIn = async (username: string, password: string) => {
+    const signIn = async (event: FormEvent, username: string, password: string) => {
+        event.preventDefault();
+
         try {
             const user = await Auth.signIn(username, password);
             if (!props.onStateChange) {
@@ -53,33 +55,36 @@ export const CustomSignIn = (props: IAuthenticatorProps) => {
                     <Col lg="6" sm="12">
                         <Card className="signInCard">
                             <Card.Body className="signInCardBody">
-                                <Row>
-                                    <Col>
-                                        <label htmlFor="email">Email</label>
-                                        <InputGroup className="mb-3">
-                                            <FormControl
-                                                id="email"
-                                                type="email"
-                                                placeholder="Enter your email address"
-                                                onChange={e => setUsername(e.target.value)} />
-                                        </InputGroup>
+                                <form onSubmit={(e) => signIn(e, username, password)}>
+                                    <Row>
+                                        <Col>
+                                            <label htmlFor="email">Email</label>
+                                            <InputGroup className="mb-3">
+                                                <FormControl
+                                                    id="email"
+                                                    type="email"
+                                                    placeholder="Enter your email address"
+                                                    onChange={e => setUsername(e.target.value)} />
+                                            </InputGroup>
 
-                                        <label htmlFor="email">Password</label>
-                                        <InputGroup className="mb-3">
-                                            <FormControl
-                                                id="email"
-                                                type="password"
-                                                placeholder="Enter your password"
-                                                onChange={e => setPassword(e.target.value)} />
-                                        </InputGroup>
-                                    </Col>
-                                </Row>
+                                            <label htmlFor="email">Password</label>
+                                            <InputGroup className="mb-3">
+                                                <FormControl
+                                                    id="email"
+                                                    type="password"
+                                                    placeholder="Enter your password"
+                                                    onChange={e => setPassword(e.target.value)} />
+                                            </InputGroup>
+                                        </Col>
+                                    </Row>
 
-                                <Row>
-                                    <Col>
-                                        <button className="w-25" type="button" onClick={() => signIn(username, password)}>Sign In</button>
-                                    </Col>
-                                </Row>
+                                    <Row>
+                                        <Col>
+                                            <Button type="submit">Login</Button>
+                                            {/* <button className="w-25" type="button" onClick={() => signIn(username, password)}>Sign In</button> */}
+                                        </Col>
+                                    </Row>
+                                </form>
                             </Card.Body>
                         </Card>
                     </Col>
