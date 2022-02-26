84116b92d5a64227f225595b2e12d710db28ef40
werzl <adam@houseofhewitt.co.uk>
2022-02-26 00:46:51 +0000

    add minecraft server table

--- src/views/Minecraft/Minecraft.tsx
+++ src/views/Minecraft/Minecraft.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { API, Auth } from 'aws-amplify';
+import { Row, Col, Table, Container, Button } from "react-bootstrap";
+import { Link, Route } from "react-router-dom";
+
+import Server from "./Server/MinecraftServer";
+
+import Api from "../../models/Api/Api";
 import { MinecraftServer, RunningServer } from "../../models/Minecraft/MinecraftServer";
 
 export interface MinecraftProps {
-    user: any
+    baseUrl: "/Minecraft"
 }
 
 export type ListServersResponse = {
@@ -11,40 +17,135 @@ export type ListServersResponse = {
     running: RunningServer[]
 }
 
-const Minecraft = () => {
+const Minecraft = (props: MinecraftProps) => {
     const [servers, setServers] = useState<ListServersResponse | null>(null);
 
+    const getData = async () => {
+        try {
+            const user = await Auth.currentAuthenticatedUser();
+
+            const apiName = Api.Name;
+            const path = Api.Minecraft.ListServersEndpoint;
+            const init = {
+                headers: {
+                    Authorization: `Bearer ${user.signInUserSession.idToken.jwtToken}`, // get jwtToken
+                },
+            };
+
+            const response = await API.get(apiName, path, init);
+
+            setServers(JSON.parse(response.body));
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    const startServer = async (serverName: string) => {
+        console.error("not implemented");
+        
+        // try {
+
+        //     const user = await Auth.currentAuthenticatedUser();
+
+        //     const apiName = "team_casual";
+        //     const path = `/minecraft/servers/start?serverName=${serverName}`;
+        //     const init = {
+        //         headers: {
+        //             Authorization: `Bearer ${user.signInUserSession.idToken.jwtToken}`, // get jwtToken
+        //         }
+        //     };
+
+        //     const response = await API.post(apiName, path, init);
+
+        //     await getData();
+        // }
+        // catch (e) {
+        //     console.error(e);
+        // }
+    };
+
     useEffect(() => {
-        const getData = async () => {
-            try {
-                const user = await Auth.currentAuthenticatedUser();
-
-                const apiName = 'team_casual';
-                const path = '/minecraft/servers';
-                const init = {
-                    headers: {
-                        Authorization: `Bearer ${user.signInUserSession.idToken.jwtToken}`, // get jwtToken
-                    },
-                };
-
-                const response = await API.get(apiName, path, init);
-
-                setServers(JSON.parse(response.body));
-            }
-            catch (e) {
-                console.error(e);
-            }
+        const onLoad = async () => {
+            await getData();
         }
 
-        getData();
+        onLoad();
     }, []);
 
     return (
         <>
             {servers &&
-                <>
-                    {servers.stopped.map(s => s.serverName)}
-                </>
+                <Container>
+                    {servers.stopped.map(s => {
+                        return (
+                            <Route path={`${props.baseUrl}/${s.serverName.replaceAll(" ", "")}`}>
+                                <Server server={s} />
+                            </Route>
+                        );
+                    })}
+
+                    <Route exact path="/Minecraft">
+                        <Row>
+                            <Col>
+                                <Table>
+                                    <thead>
+                                        <tr>
+                                            <th>Server Name</th>
+                                            <th>Type</th>
+                                            <th>Minecraft Version</th>
+                                            <th>Region</th>
+                                            <th>State</th>
+                                            <th>DNS</th>
+                                            <th>IP Addr</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        {servers.running.map(s => {
+                                            return (
+                                                <tr>
+                                                    <td>
+                                                        <Link to={`/minecraft/${s.serverName.replaceAll(" ", "")}`}>
+                                                            <p>{s.serverName}</p>
+                                                        </Link>
+                                                    </td>
+                                                    <td>{s.serverType}</td>
+                                                    <td>{s.minecraftVersion}</td>
+                                                    <td>{s.availabilityZone}</td>
+                                                    <td>{s.instanceState}</td>
+                                                    <td>{s.publicDnsName}</td>
+                                                    <td>{s.publicIpAddress}</td>
+                                                </tr>
+                                            );
+                                        })}
+
+                                        {servers.stopped.map(s => {
+                                            return (
+                                                <tr>
+                                                    <td>
+                                                        <Link to={`/Minecraft/${s.serverName.replaceAll(" ", "")}`}>
+                                                            <p>{s.serverName}</p>
+                                                        </Link>
+                                                    </td>
+                                                    <td>{s.serverType}</td>
+                                                    <td>{s.minecraftVersion}</td>
+                                                    <td>{s.availabilityZone}</td>
+                                                    <td>{s.instanceState}</td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td><Button onClick={() => startServer(s.serverName)}>Start</Button></td>
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                </Table>
+                            </Col>
+                        </Row>
+                    </Route>
+
+                </Container>
             }
         </>
     );
